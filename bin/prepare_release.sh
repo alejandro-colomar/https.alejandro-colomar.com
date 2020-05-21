72f46b6416414874e72106b5d144b4b28224b695
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-05-21 18:27:59 +0200

    Fix prepare_release.sh missing "

--- bin/prepare_release.sh
+++ bin/prepare_release.sh
@@ -18,7 +18,7 @@
 ################################################################################
 update_version()
 {
-	local	old_version="0.4"
+	local	old_version="0.5"
 	local	version=$1
 
 	sed "/--branch master/s/master/v${version}/"			\
@@ -27,7 +27,7 @@ update_version()
 			-i ./Swarm/release/web.yaml
 	sed "/www.alejandro-colomar.bit:v${old_version}/s/v${old_version}/v${version}/" \
 			-i ./Swarm/release/web-BLUE.yaml
-	sed "/old_version=${old_version}/s/${old_version}/${version}/"	\
+	sed "/old_version=\"${old_version}\"/s/${old_version}/${version}/" \
 			-i ./bin/prepare_release.sh
 }
 
