fbd23bb18cbee5dd1cf271caaf023115c19728c1
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-06-15 23:26:52 +0200

    Organize /bin/

--- bin/release/version.sh
+++ bin/release/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-##	./bin/update_version.sh	[<version>]
+##	./bin/release/version.sh	[<version>]
 ################################################################################
 ##	Copyright (C) 2020	  Alejandro Colomar Andrés		      ##
 ##	SPDX-License-Identifier:  GPL-2.0-only				      ##
