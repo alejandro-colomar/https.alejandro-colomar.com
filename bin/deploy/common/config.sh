0f85cfeaef2b2dcf34b9b2a38e0eb77f505b003f
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-08-05 15:41:50 +0200

    deploy/config.sh: cp should follow symlinks

--- bin/deploy/common/config.sh
+++ bin/deploy/common/config.sh
@@ -30,14 +30,14 @@ function prepare_configs()
 {
 
 	mkdir -pv	/run/configs/;
-	cp -rfvT	run/configs/www/	/run/configs/www;
+	cp -fLrvT	run/configs/www/	/run/configs/www;
 }
 
 function prepare_secrets()
 {
 
 	mkdir -pv	/run/secrets/;
-	cp -rfvT	run/secrets/www/	/run/secrets/www;
+	cp -fLrvT	run/secrets/www/	/run/secrets/www;
 }
 
 
