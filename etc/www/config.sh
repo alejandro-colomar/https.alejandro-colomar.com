f5f6a9836009d27b1a34f04a728df2f8d1252b2c
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-08-05 15:44:14 +0200

    Branch: master

--- etc/www/config.sh
+++ etc/www/config.sh
@@ -20,9 +20,9 @@ WWW_CONFIG_H="/etc/www/config.sh included";
 WWW_PORT_EXP=32001;
 WWW_PORT_RC=31001;
 WWW_PORT_STABLE=30001;
-WWW_STABILITY="stable";	## "stable", "rc", or "exp"
+WWW_STABILITY="exp";	## "stable", "rc", or "exp"
 WWW_STACK_BASENAME="www";
-WWW_VERSION="0.16.1";	## tag name or branch name.
+WWW_VERSION="master";	## tag name or branch name.
 
 
 ################################################################################
