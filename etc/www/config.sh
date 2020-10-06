1426951a393960ca168c92a42bea7ced987955d4
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-10-06 19:31:25 +0200

    Release 0.23.4

--- etc/www/config.sh
+++ etc/www/config.sh
@@ -22,7 +22,7 @@ WWW_PORT_RC=31001;
 WWW_PORT_STABLE=30001;
 WWW_STABILITY="stable";	## "stable", "rc", or "exp"
 WWW_STACK_BASENAME="www";
-WWW_VERSION="0.23.3";	## tag name or branch name.
+WWW_VERSION="0.23.4";	## tag name or branch name.
 
 
 ################################################################################
