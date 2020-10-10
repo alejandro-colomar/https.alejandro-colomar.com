f611da8795089c3a9222f20928b5d2cb671dbe0c
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-10-11 00:49:50 +0200

    Pre-release 0.25-a1

--- etc/www/config.sh
+++ etc/www/config.sh
@@ -22,7 +22,7 @@ WWW_PORT_STABLE=30001;
 WWW_PROJECT="www";
 WWW_STABILITY="exp";	## "stable", "rc", or "exp"
 WWW_STACK="${WWW_PROJECT}-${WWW_STABILITY}";
-WWW_VERSION="alx";	## tag name or branch name.
+WWW_VERSION="0.25-a1";	## tag name or branch name.
 
 
 ################################################################################
