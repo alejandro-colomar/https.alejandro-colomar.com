93a81154445f27ce8a854726279710ebb5e53f65
Alejandro Colomar <alx.manpages@gmail.com>
2021-02-25 16:55:21 +0100

    bin/containers/build_push.sh: Simplify script
    Signed-off-by: Alejandro Colomar <alx.manpages@gmail.com>

--- bin/containers/build_push.sh
+++ bin/containers/build_push.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
-set -Eeo pipefail;
-##	./bin/containers/build_push.sh;
-################################################################################
-##      Copyright (C) 2020        Alejandro Colomar Andrés                    ##
-##      SPDX-License-Identifier:  GPL-2.0-only                                ##
-################################################################################
-##
-## Build and push the docker image
-## ===============================
-##
-################################################################################
+set -Eeuo pipefail;
+########################################################################
+#      Copyright (C) 2020        Alejandro Colomar Andres
+#      SPDX-License-Identifier:  GPL-2.0-only
+########################################################################
 
 
-ARGC=0;
-argc=$#;
-EX_USAGE=64;
-if [ ${argc} -ne ${ARGC} ]; then
-	echo	"Illegal number of parameters (Requires ${ARGC})";
+if [ $# -ne 0 ]; then
+	echo	"Usage: ${BASH_SOURCE[0]}";
 	exit	${EX_USAGE};
-fi
+fi;
 
 
 . etc/www/config.sh;
 
+
 docker build -t "${WWW_DK_REPO}/${WWW_DK_IMG}:${WWW_DK_TAG}" .;
 docker push "${WWW_DK_REPO}/${WWW_DK_IMG}:${WWW_DK_TAG}";
-
-
-################################################################################
-##	end of file							      ##
-################################################################################
