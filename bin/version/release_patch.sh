c396fb51fc454d3dbb651f37c286012bd26de597
Alejandro Colomar <alx.manpages@gmail.com>
2021-03-17 16:57:33 +0100

    bin/version/release_patch.sh: New script to release patches (x.y.Z), using the same x.y.0 docker image
    Signed-off-by: Alejandro Colomar <alx.manpages@gmail.com>

---
+++ bin/version/release_patch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -Eeuo pipefail;
+########################################################################
+#      Copyright (C) 2020        Alejandro Colomar Andres
+#      SPDX-License-Identifier:  GPL-2.0-only
+########################################################################
+
+
+EX_OK=0;
+EX_USAGE=64;
+EX_CANTCREAT=73;
+
+if [ $# -ne 1 ]; then
+	>&2 echo "Usage: ${BASH_SOURCE[0]} <version>";
+	exit ${EX_USAGE};
+fi;
+version="$1";
+
+if git rev-parse "refs/tags/${version}" >/dev/null 2>&1; then
+	>&2 echo "Version already exists!";
+	exit ${EX_CANTCREAT};
+fi;
+
+git tag -a ${version} -m "";
+
+exit ${EX_OK};
